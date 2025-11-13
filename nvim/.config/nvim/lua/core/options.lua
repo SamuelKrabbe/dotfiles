4303a35d921ca64d0d48de9562b06c4bbe921f28
SamuelKrabbe <krabbe.samuel@gmail.com>
2025-11-13 10:04:07 -0400

    chore: update tmux status bar

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -2,8 +2,8 @@
 vim.opt.number = true                              -- Line numbers
 vim.opt.relativenumber = true                      -- Relative line numbers
 vim.opt.cursorline = true                          -- Highlight current line
-vim.opt.wrap = true                               -- Don't wrap lines
-vim.opt.scrolloff = 8                             -- Keep 10 lines above/below cursor 
+vim.opt.wrap = true                                -- Wrap lines
+vim.opt.scrolloff = 8                              -- Keep 10 lines above/below cursor 
 vim.opt.sidescrolloff = 8                          -- Keep 8 columns left/right of cursor
 
 -- Indentation
@@ -22,7 +22,7 @@ vim.opt.incsearch = true                           -- Show matches as you type
 
 -- Visual settings
 vim.opt.termguicolors = true                       -- Enable 24-bit colors
-vim.opt.signcolumn = "yes"                         -- Always show sign column
+vim.opt.signcolumn = "no"                          -- Show sign column
 vim.opt.colorcolumn = "120"                        -- Show column at 120 characters
 vim.opt.showmatch = true                           -- Highlight matching brackets
 vim.opt.matchtime = 2                              -- How long to show matching bracket

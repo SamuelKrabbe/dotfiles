7461ebde20673c4a4a519b52fd384728a75f4368
SamuelKrabbe <krabbe.samuel@gmail.com>
2025-11-13 13:36:32 -0400

    feat: nvim autocomplete and autopair

--- nvim/.config/nvim/lua/plugins/one-liners.lua
+++ nvim/.config/nvim/lua/plugins/one-liners.lua
@@ -1,17 +1,34 @@
 return {
-	{ -- This helps with ssh tunneling and copying to clipboard
-		"ojroques/vim-oscyank",
-	},
-	{ -- Show historical versions of the file locally
-		"mbbill/undotree",
+    { -- This helps with ssh tunneling and copying to clipboard
+        "ojroques/vim-oscyank",
+    },
+    { -- Show historical versions of the file locally
+        "mbbill/undotree",
         config = function()
             vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = "Toggle undotree" } )
         end
-	},
-	{ -- Show CSS Colors
-		"brenoprata10/nvim-highlight-colors",
-		config = function()
-			require("nvim-highlight-colors").setup({})
-		end,
-	},
+    },
+    { -- Show CSS Colors
+        "brenoprata10/nvim-highlight-colors",
+        config = function()
+            require("nvim-highlight-colors").setup({})
+        end,
+    },
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            local npairs = require("nvim-autopairs")
+            npairs.setup({
+                check_ts = true, -- enables smarter pairs using treesitter
+                fast_wrap = {},
+                disable_filetype = { "TelescopePrompt", "vim" },
+            })
+        end,
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        event = "InsertEnter",
+        config = true,
+    }
 }

79f33f6f36faecda3ea2581f511f404fb4ad7fb7
SamuelKrabbe <krabbe.samuel@gmail.com>
2026-02-14 08:44:48 -0400

    chore: add greeting function to fish

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,48 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-    end
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+		local builtin = require("telescope.builtin")
+
+		telescope.setup({
+			defaults = {
+				prompt_prefix = "🔍 ",
+				selection_caret = "➤ ",
+				path_display = { "smart" },
+
+				preview = {
+					treesitter = false,
+				},
+
+				mappings = {
+					i = {
+						["<C-j>"] = actions.move_selection_next,
+						["<C-k>"] = actions.move_selection_previous,
+						["<CR>"] = actions.select_default,
+						["<C-c>"] = actions.close,
+					},
+					n = {
+						["q"] = actions.close,
+					},
+				},
+			},
+
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+				git_files = {
+					show_untracked = true,
+				},
+			},
+		})
+
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+	end,
 }
